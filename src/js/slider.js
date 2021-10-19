642ccfd0b2d0f56633b86d70514387c0ae3b7848
fessenko-tr <fessenko.tr@gmail.com>
2021-10-20 00:22:48 +0300

    добавляет наброски слайдера

---
+++ src/js/slider.js
@@ -0,0 +1,50 @@
+var $status = $('.paging-info');
+var $slickElement = $('.reviews-list');
+
+var scrollCount = null;
+var scroll = null;
+
+$slickElement.slick({
+  slidesToShow: 1,
+  centerPadding: '190px',
+  centerMode: true,
+  infinite: false,
+  arrows: false,
+  initialSlide: 1,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        centerMode: false,
+        centerPadding: '0px',
+      },
+    },
+  ],
+});
+
+$slickElement.on('wheel', function (e) {
+  e.preventDefault();
+
+  clearTimeout(scroll);
+  scroll = setTimeout(function () {
+    scrollCount = 0;
+  }, 200);
+  if (scrollCount) return 0;
+  scrollCount = 1;
+
+  if (e.originalEvent.deltaY < 0) {
+    $(this).slick('slickNext');
+  } else {
+    $(this).slick('slickPrev');
+  }
+});
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  $status.text(i + '/' + slick.slideCount);
+});
+
+$slickElement.on('edge', function () {
+  $slickElement.slick('slickGoTo', 0);
+});
