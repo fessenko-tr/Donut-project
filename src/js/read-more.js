9f2006a92a1241d03fa42b81aa9428bee62bfed9
pekosk <rkostyk64@gmail.com>
2021-10-20 11:40:47 +0300

    Работающий скрипт кнопок

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,38 +1,17 @@
-function tab(but_tab, hidden_block_tab, icon_tab, text_block, before_text, after_text) {
-    but_tab.addEventListener("click", function() {
-        if(hidden_block_tab.classList.contains('hidden_block')) {
-            hidden_block_tab.classList.remove('hidden_block');
-            hidden_block_tab.classList.add('visible_block');
-
-            if(icon_tab) {
-                icon_tab.classList.remove('fa-chevron-down');
-                icon_tab.classList.add('fa-chevron-up');
-            }
-            /*Делаем проверку на заполненность переменной*/
-            if(text_block) {
-                /*Переписываем текст внутри элемента*/
-                text_block.innerHTML= after_text;
-            }
-        }
-        else if(hidden_block_tab.classList.contains('visible_block')) {
-            hidden_block_tab.classList.remove('visible_block');
-            hidden_block_tab.classList.add('hidden_block');
-
-            if(icon_tab) {
-                icon_tab.classList.remove('fa-chevron-up');
-                icon_tab.classList.add('fa-chevron-down');
-            }
-            if(text_block) {
-                text_block.innerHTML= before_text;
-            }
-        }
-    }); 
+const refs = {
+    readMoreBtn: [...document.querySelectorAll('.read-more-btn')],
 }
 
-var download_more = document.querySelector('.container_for_tab .read-more-btn');
-var hidden_list = document.querySelector('.container_for_tab .tab_content');
-var icon_more = document.querySelector('.container_for_tab .icon_more');
-/*добавим переменную с элементом в котором нужно менять текст*/
-var more_text = document.querySelector('.container_for_tab .text_more');
+refs.readMoreBtn.forEach(e => {
+    e.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.textContent === 'Read more') {
+            target.textContent = 'Less';
+        } else {
+            target.textContent = 'Read more';
+        }
+        target.previousElementSibling.classList.toggle('read-more-open');
+    })
+})
 
-tab(download_more, hidden_list, icon_more, more_text, "Read more", "Less");
+
